a13b36ff8893222833025cf4120293573d70253c
Lucas Butron Caballero <lucaswas7@gmail.com>
2017-04-06 02:01:04 +0200

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+ protect_from_forgery with: :exception
+ def goodbye
+ render html: "goodbye, world!"
+ end
+ def hello
+ render html: "hola, mundo!"
+ end
+ 
+end
